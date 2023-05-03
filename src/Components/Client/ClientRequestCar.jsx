dc2298594cdf5cabe3487ad13a4da6c9768cfee0
Tamer Boutros <thb08@mail.aub.edu>
2023-05-04 02:14:06 +0300

    implemented valet functionality

--- src/Components/Client/ClientRequestCar.jsx
+++ src/Components/Client/ClientRequestCar.jsx
@@ -1,68 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { useState } from 'react';
 
+const SERVER_URL = 'http://127.0.0.1:3500';
 
 const RequestCar = () => {
-    const [TicketNumberInput, setDisplayTicketNumberInput] = useState('');
-    const [display, setDisplay] = useState('');
-    const [isShown, SetIsShown] = useState(false)
-    const [isHidden, SetIsHidden] = useState(true)
+  const [ticketNumberInput, setTicketNumberInput] = useState('');
+  const [display, setDisplay] = useState('');
+  const [isShown, setIsShown] = useState(false);
+  const [isHidden, setIsHidden] = useState(true);
 
-    const OrderCarChange = (event) => {
-        setDisplayTicketNumberInput(event.target.value)
+  const handleTicketNumberChange = (event) => {
+    setTicketNumberInput(event.target.value);
+  };
+
+  const handleOrderCarClick = async () => {
+    const confirm = window.confirm('Are you sure this is your ticket number?');
+    if (confirm) {
+      setIsHidden(false);
+      setIsShown(true);
+
+      // Make API call to create the car request
+      try {
+        const response = await fetch(`${SERVER_URL}/valet_create_request`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ car_number: ticketNumberInput }),
+        });
+        const data = await response.json();
+        setDisplay(data.car_number);
+      } catch (error) {
+        console.error(error);
+      }
     }
+  };
 
-    const OrderCarClicked = () => {
-        var confirm = window.confirm("Are you sure this is your ticket number?")
-        if (confirm) {
-            setDisplay(TicketNumberInput);
-            SetIsShown(true);
-            SetIsHidden(false);
-        } else {
-        }
-        setDisplay(TicketNumberInput);
+  return (
+    <div className="container-sm">
+      {isHidden && (
+        <>
+          <div className="form-floating mb-3 align-items-center">
+            <input
+              value={ticketNumberInput}
+              onChange={handleTicketNumberChange}
+              type="number"
+              className="form-control"
+              id="TicketNumberInput"
+              name="TicketNumberInput"
+              placeholder="Ticket Numbers"
+            />
+            <label htmlFor="floatingInput">Ticket Number</label>
+          </div>
+          <div className="d-grid gap-2 col-6 mx-auto">
+            <button className="btn btn-outline-primary" type="button" onClick={handleOrderCarClick}>
+              Order Car
+            </button>
+          </div>
+        </>
+      )}
 
+      <br />
 
-    };
-
-    return (
-
-        <div class="container-sm">
-
-            {isHidden && (
-                <><div class="form-floating mb-3 align-items-center">
-                    <input value={TicketNumberInput} onChange={OrderCarChange} type="number" class="form-control" id="TicketNumberInput" name="TicketNumberInput" placeholder='Ticket Numbers' />
-                    <label for="floatingInput">Ticket Number</label>
-                </div><div class="d-grid gap-2 col-6 mx-auto">
-                        <button class="btn btn-outline-primary" type="button" onClick={OrderCarClicked}>Order Car</button>
-                    </div></>
-            )}
-
-            <br></br>
-
-            {isShown && (
-                <div id="DisplayDiv">
-                    <h3>Your car is on the way!</h3>
-                    <br></br>
-                    <div class="spinner-grow text-primary" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                    <br></br>
-                    <br></br>
-                    <h2>Please show the valet attendant the number displayed below: </h2>
-                    <font size="+10">{display}</font>
-                </div>
-            )}
-
-
-
+      {isShown && (
+        <div id="DisplayDiv">
+          <h3>Your car is on the way!</h3>
+          <br />
+          <div className="spinner-grow text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <br />
+          <br />
+          <h2>Please show the valet attendant the number displayed below:</h2>
+          <font size="+10">{display}</font>
         </div>
+      )}
+    </div>
+  );
+};
 
-
-
-
-    )
-
-}
 export default RequestCar;
--- src/Components/Valet/ValetGetCar.jsx
+++ src/Components/Valet/ValetGetCar.jsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://127.0.0.1:3500';
 
 function ValetGetCar() {
+    const [requests, setRequests] = useState([]);
+
+    useEffect(() => {
+        // make API call to get all getcar requests
+        axios.get(`${SERVER_URL}/valet_get_car_requests`)
+            .then(response => {
+                setRequests(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleDoneClick = (requestId) => {
+        // make API call to delete the request
+        axios.delete(`${SERVER_URL}/valet_delete_request/${requestId}`)
+            .then(response => {
+                // remove the request from the list of requests
+                setRequests(requests.filter(request => request.id !== requestId));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    // poll for new requests every 10 seconds
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            axios.get(`${SERVER_URL}/valet_get_car_requests`)
+                .then(response => {
+                    setRequests(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }, 10000);
+
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
-        <div className='container-sm' style={{justifyContent:"center", display:"flex"}}>
-            <div class="card border-primary" style={{width: "18rem"}}>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Get car #</li>
-                    <button className='btn btn-primary'>Done</button>
-                    <br></br>
-                    <li class="list-group-item">Get car #</li>
-                    <button className='btn btn-primary'>Done</button>
-                    <br></br>
-                    <li class="list-group-item">Get car #</li>
-                    <button className='btn btn-primary'>Done</button>
-                    <br></br>
-                </ul>
-            </div>
-        </div>
+        <div className='container-sm' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            {requests.map(request => (
+                <div className="card border-primary" key={request.id} style={{ width: "18rem", marginBottom: '1rem' }}>
+                    <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Get car #{request.car_number}</li>
 
-    )
+                        <button className='btn btn-primary' onClick={() => handleDoneClick(request.id)}>Done</button>
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
 }
+
 export default ValetGetCar;
+
